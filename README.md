2e1802075eaba0086866df5cdec93a879c276662
Freiberger <marcos.freiberger@capgemini.com>
2022-02-21 09:34:31 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+<h1 align = "center"> :fast_forward: Dogs - Redux :rewind: </h1>
+
+Para visualizar o projeto acesse:
+
+## 📖 Sobre
+
+<p>Refatoração com Redux do site Dogs desenvolvido no curso de React Completo - Origamid.</p>
+
+---
+
+## 🛠 Tecnologias utilizadas
+
+Frontend
+
+- HTML
+- CSS
+- JavaScript
+- React
+- Redux
+
+---
+
+Developed with 💙 by Marcos N. Freiberger
---
+++ src/Components/Photo/PhotoDelete.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import styles from './PhotoDelete.module.css';
+import useFetch from './../../Hooks/useFetch';
+import { PHOTO_DELETE } from '../../api';
+
+const PhotoDelete = ({ id }) => {
+  const { loading, request } = useFetch();
+
+  async function handleClick() {
+    const confirm = window.confirm('Tem certeza que deseja deletar?');
+    if (confirm) {
+      const { url, options } = PHOTO_DELETE(id);
+      const { response } = await request(url, options);
+      if (response.ok) window.location.reload();
+    }
+  }
+
+  return (
+    <div>
+      {loading ? (
+        <button className={styles.delete} disabled>
+          Deletar
+        </button>
+      ) : (
+        <button className={styles.delete} onClick={handleClick}>
+          Deletar
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default PhotoDelete;
